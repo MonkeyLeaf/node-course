0858c00859b927cdcf69542d950ed5364a72e9e5
tuancm <caotuan92.hus@gmail.com>
2020-08-24 11:51:58 +0700

    update

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -15,7 +15,7 @@ router.post('/users', async (req, res) => {
 
 router.post('/users/login', async (req, res) => {
     try {
-        const user = await User.findByCredentRead123456ials(req.body.email, req.body.password)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
         res.send(user)
     } catch (e) {
         res.status(400).send()
